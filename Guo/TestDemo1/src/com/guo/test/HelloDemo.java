3b3a426949a9957fee76559df7af404b9c99588a
Ethanyan <yanyuliang6@163.com>
2019-09-11 14:50:12 +0800

    测试helloworld

---
+++ Guo/TestDemo1/src/com/guo/test/HelloDemo.java
@@ -0,0 +1,13 @@
+package com.guo.test;
+
+/* public 访问修饰符
+class 类
+HelloDemo 类名 名字以字母开头，后面可以跟字母和数字的任意组合，每个单词首字母大写
+注意：源代码文件名必须与公共类的名字相同，并用 .java 作为扩展名
+
+ */
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
