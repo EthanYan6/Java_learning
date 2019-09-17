5dea996d27518b241a6efe8c9c48567af38be17d
Ethanyan <yanyuliang6@163.com>
2019-09-17 15:58:41 +0800

    添加学习笔记，放置在readme中

--- Guo/TestDemo1/src/com/guo/test/HelloDemo.java
+++ Guo/TestDemo1/src/com/guo/test/HelloDemo.java
@@ -1,13 +1,22 @@
 package com.guo.test;
 
-/* public 访问修饰符
-class 类
-HelloDemo 类名 名字以字母开头，后面可以跟字母和数字的任意组合，每个单词首字母大写
-注意：源代码文件名必须与公共类的名字相同，并用 .java 作为扩展名
 
+/** public 访问修饰符
+ * class 类
+ * HelloDemo 类名 名字以字母开头，后面可以跟字母和数字的任意组合，每个单词首字母大写
+ * 注意：源代码文件名必须与公共类的名字相同，并用 `.java` 作为扩展名
+ * 块都是以 { } 开始和结尾的
  */
+
 public class HelloDemo {
     public static void main(String[] args) {
+        // 尝试 println 方法
         System.out.println("hello world");
+        System.out.println("Ethan");
+        // 尝试 print 方法
+        System.out.print("hello world");
+        System.out.print("Ethan");
+
     }
 }
+
