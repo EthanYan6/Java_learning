e39abe76bcce00b9c07d02cf19153e41cc9e5ccf
Ethanyan <yanyuliang6@163.com>
2019-09-19 16:21:27 +0800

    添加运算符、循环、条件判断等学习笔记

--- Guo/TestDemo1/src/com/guo/test/HelloDemo.java
+++ Guo/TestDemo1/src/com/guo/test/HelloDemo.java
@@ -11,18 +11,77 @@ package com.guo.test;
 public class HelloDemo {
     public static void main(String[] args) {
         // 尝试 println 方法
-        System.out.println("hello world");
-        System.out.println("Ethan");
+//        System.out.println("hello world");
+//        System.out.println("Ethan");
         // 尝试 print 方法
 //        System.out.print("hello world");
 //        System.out.print("Ethan");
 
         // 变量
-        int a = 1;
-        System.out.println(a);
-        float nice;
-        nice = 0.1f;
-        System.out.println(nice);
+//        int a = 1;
+//        System.out.println(a);
+//        float nice;
+//        nice = 0.1f;
+//        System.out.println(nice);
+
+        // 运算符
+//        int i1 = 10;
+//        int i2 = 22;
+//        int i3 = 25;
+//        int i4 = 66;
+//        System.out.println("i1 + i2 = " + (i1 + i2));
+//        System.out.println("i3 - i2 = " + (i3 - i2));
+//        System.out.println("i4 * i1 = " + (i4 - i1));
+//        System.out.println("i2 / i1 = " + (i2 / i1));
+
+        // 自增自减
+//        ++i1;
+//        System.out.println(i1);
+//
+//        --i1;
+//        System.out.println(i1);
+//
+//        int b = i1--;
+//        System.out.println(i1);
+//        System.out.println(b);
+
+        // 关系运算符
+//        int s = 10;
+//        int n = 20;
+//        System.out.println("s == n = " + (s == n) );
+//        System.out.println("s != n = " + (s != n) );
+//        System.out.println("s > n = " + (s > n) );
+//        System.out.println("s < n = " + (s < n) );
+//        System.out.println("n >= s = " + (n >= s) );
+//        System.out.println("n <= s = " + (n <= s) );
+
+        // while 循环
+//        int int1 = 10;
+//        while (int1 > 0) {
+//            System.out.println(int1);
+//            int1--;
+//        }
+        // do...while 循环
+//        int a = 10;
+//        do {
+//            System.out.println(a);
+//            a--;
+//        }while (a<0);
+        // for 循环
+//        for (int x = 0; x < 3; x = x + 1){
+//            System.out.println(x);
+//        }
+        // 增强for循环
+//        String [] namelist = {"Ethan", "yan", "liang"};
+//        for (String name: namelist) {
+//            System.out.print(name);
+//            System.out.print(",");
+//        }
+        // if ... else
+        int i = 1;
+        if(i==1){
+            System.out.println("值为1");
+        }
     }
 }
 
